f00ab7f2c9f6317b6315fbe8d016dd35c7aa9bf5
smissu <eric.smith10@gmail.com>
2023-06-18 11:30:20 +0000

    Added additional render page and API endpoint

--- app.py
+++ app.py
@@ -1,11 +1,48 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Bangaluru, India',
+    'salary': 'Rs. 10,00,000'
+  },
+  {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Delhi, India',
+    'salary': 'Rs. 15,00,000'
+  },
+  {
+    'id': 3,
+    'title': 'Front End Engineer',
+    'location': 'Remote',
+
+  },
+  {
+    'id': 4,
+    'title': 'Bank End Engineer',
+    'location': 'San Francisco, USA',
+    'salary': '$ 120,000'
+  },
+  
+]
+
+company_name="EZE"
+
 
 @app.route('/')
 def hello_world():
   #return "<p>Hello World</p>"
-  return render_template('home.html')
+  return render_template('home.html', 
+                         jobs=JOBS, 
+                         company_name="EZE")
+
+@app.route('/api/jobs')
+def list_jobs():
+  return jsonify(JOBS)
   
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
